1f85508509373430208182e3df095dee6f106057
innocent2102 <michal2102@gmail.com>
2017-12-28 20:42:41 +0100

    Moved appComponent content to menuComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
-import { Iuser } from './users/iuser';
-import { UsersService } from './users/users.service';
-import { Igroup } from './groups/igroup';
-import { GroupsService } from './groups/groups.service';
-
-
-
 
 @Component({
   selector: 'app-root',
@@ -16,75 +9,9 @@ import { GroupsService } from './groups/groups.service';
 })
 export class AppComponent implements OnInit {
 
-    usersList: Iuser[];
-    groupsList: Igroup[];
-
-    newGroupForm: FormGroup;
-    newUserForm: FormGroup;
-
-    constructor(private _usersService: UsersService,
-        private _groupsService: GroupsService,
-        private _formBuilder: FormBuilder) {
-            this.newGroupForm = _formBuilder.group({
-                name: ['', Validators.required]
-            });
-            this.newUserForm = _formBuilder.group({
-                name: ['', Validators.required],
-                email: ['', Validators.required],
-            });
-    }
-
-    addNewGroup() {
-        this._groupsService.insertToGroupList(this.newGroupForm.value)
-            .subscribe(
-                group => {
-                    alert('Grupa dodana!');
-                    console.log(this.newGroupForm);
-                },
-                error => console.log(error)
-            );
-    }
-
-    removeGroup(groupId) {
-        this._groupsService.removeGroup(groupId)
-            .subscribe(
-                 user => {
-                    console.log(user);
-                 },
-                 error => console.log(error)
-             );
-    }
-    addNewUser() {
-        this._usersService.insertToUsersList(this.newUserForm.value)
-            .subscribe(
-                group => {
-                    alert('Użytkownik dodany!');
-                    console.log(this.newUserForm);
-                },
-                error => console.log(error)
-            );
-    }
-
-    removeUser(userId) {
-        this._usersService.removeUser(userId)
-            .subscribe(
-                 user => {
-                    console.log(user);
-                 },
-                 error => console.log(error)
-             );
-    }
-   
+    constructor() { }
 
     ngOnInit() {
-        this._usersService.getUsersList()
-        .subscribe(users =>
-            this.usersList = users['records']
-        );
-        this._groupsService.getGroupsList()
-        .subscribe(groups =>
-            this.groupsList = groups['records']
-        );
 
     }
 
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Iuser } from '../users/iuser';
+import { UsersService } from '../users/users.service';
+import { Igroup } from '../groups/igroup';
+import { GroupsService } from '../groups/groups.service';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+
 
 @Component({
   selector: 'app-menu',
@@ -7,9 +13,80 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+    usersList: Iuser[];
+    groupsList: Igroup[];
+
+    newGroupForm: FormGroup;
+    newUserForm: FormGroup;
+
+    constructor(private _usersService: UsersService,
+        private _groupsService: GroupsService,
+        private _formBuilder: FormBuilder) {
+            this.newGroupForm = _formBuilder.group({
+                name: ['', Validators.required]
+            });
+            this.newUserForm = _formBuilder.group({
+                name: ['', Validators.required],
+                email: ['', Validators.required],
+            });
+    }
+
+    addNewGroup() {
+        this._groupsService.insertToGroupList(this.newGroupForm.value)
+            .subscribe(
+                group => {
+                    alert('Grupa dodana!');
+                    console.log(this.newGroupForm);
+                },
+                error => console.log(error)
+            );
+    }
+
+    removeGroup(groupId) {
+        this._groupsService.removeGroup(groupId)
+            .subscribe(
+                 user => {
+                    console.log(user);
+                 },
+                 error => console.log(error)
+             );
+    }
+    addNewUser() {
+        this._usersService.insertToUsersList(this.newUserForm.value)
+            .subscribe(
+                group => {
+                    alert('Użytkownik dodany!');
+                    console.log(this.newUserForm);
+                },
+                error => console.log(error)
+            );
+    }
+
+    removeUser(userId) {
+        this._usersService.removeUser(userId)
+            .subscribe(
+                 user => {
+                    console.log(user);
+                 },
+                 error => console.log(error)
+             );
+    }
+
+
+
+
+
 
   ngOnInit() {
-  }
+        this._usersService.getUsersList()
+        .subscribe(users =>
+            this.usersList = users['records']
+        );
+        this._groupsService.getGroupsList()
+        .subscribe(groups =>
+            this.groupsList = groups['records']
+        );
+
+     }
 
 }
